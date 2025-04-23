c896e9a227bd69a06e2f50cdbaae998f862d1e1f
reanchanrin12 <ooppa5396@gmail.com>
2025-04-23 13:22:52 +0700

    responsive login | register

--- src/auth/login/index.jsx
+++ src/auth/login/index.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import LoginForm from '../../components/authComponent/LoginForm';
-import pic from '../../assets/authImage/Mobile login-rafiki.png'
+import pic from '../../assets/authImage/Mobile login-rafiki.png';
+
 const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center  px-4 py-12 font-[Suwannaphum]">
-      <div className="flex w-full max-w-6xl rounded-3xl shadow-lg   bg-[#fff7f5]">
+    <div className="min-h-screen flex items-center justify-center px-4 py-12 font-[Suwannaphum]">
+      <div className="flex flex-col md:flex-row w-full max-w-6xl rounded-3xl shadow-lg bg-[#fff7f5]">
         
-        <div className="w-1/2 hidden md:flex">
-          <img 
+        
+        <div className="hidden md:flex w-1/2 items-center justify-center p-8">
+          <img
             src={pic}
             alt="Login background"
-            className=" w-full object-cover "
+            className="w-full max-h-[500px] object-contain transition-transform duration-300"
           />
-        </div>      
+        </div>
+
+      
         <div className="w-full md:w-1/2 p-10 flex items-center justify-center bg-[#fff7f5]">
           <LoginForm />
         </div>
-        
+
       </div>
     </div>
   );
--- src/auth/register/index.jsx
+++ src/auth/register/index.jsx
@@ -4,20 +4,21 @@ import pic from '../../assets/authImage/Sign up-amico.png';
 
 const Register = () => {
   return (
-    <div className="min-h-screen overflow-hidden flex items-center justify-center  px-4 ">
-      <div className="flex w-full max-w-6xl rounded-3xl shadow-lg   bg-[#fcf5f5]">
-        {/* Left Side Image */}
-        <div className="w-1/2 overflow-hidden md:flex items-center justify-center bg-[#fcf5f5] p-8  ">
+    <div className="min-h-screen overflow-hidden flex items-center justify-center px-4">
+      <div className="flex flex-col md:flex-row w-full max-w-6xl rounded-3xl shadow-lg bg-[#fcf5f5]">
+        
+        
+        <div className="hidden md:flex w-1/2 overflow-hidden items-center justify-center bg-[#fcf5f5] p-8">
           <img 
             src={pic} 
             alt="Register" 
-            className="w-full h-auto max-h-[500px]  object-contain transition-transform duration-300 "
+            className="w-full h-auto max-h-[500px] object-contain transition-transform duration-300"
           />
         </div>
 
-
-        <div className="w-full md:w-1/2 flex items-center justify-center bg-[#fcf5f5]  px-8 overflow-hidden">
-          <div className="w-full max-w-md ">
+        {/* Right Side Form */}
+        <div className="w-full md:w-1/2 flex items-center justify-center bg-[#fcf5f5] px-8 py-12">
+          <div className="w-full max-w-md">
             <RegisterForm />
           </div>
         </div>
